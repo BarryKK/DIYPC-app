d377a5b6479d54a8e00c19e117d9d485194e88ab
Steve <pyk3201k@gamil.com>
2020-12-30 16:04:21 +0800

    first commit

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <div>
+      <img
+        src="../..//assets/icons/icons8-menu-144.png"
+        style="width: 58px; height: 58px"
+      />
+      <h1>DIY装机</h1>
+    </div>
+    <ul>
+      <li>登入</li>
+      <li>登出</li>
+      <li>注册</li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  width: 90%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+
+div {
+  display: flex;
+  align-items:center;
+  img {
+    margin: 0 1rem;
+  }
+}
+
+nav ul {
+  width: 15%;
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+  list-style: none;
+}
+</style>
