557089c7743a735c7f85dda99bb5ef502f28a69d
Steve <pyk3201k@gamil.com>
2021-01-25 17:36:12 +0800

    validation

---
+++ src/components/layout/TextInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="TextInput"
+    :class="{ 'has-error': !!errorMessage, success: meta.valid }"
+  >
+    <label :for="name">{{ label }}</label>
+    <input
+      :name="name"
+      :id="name"
+      :type="type"
+      :value="inputValue"
+      @input="handleChange"
+      @blur="hand"
+    />
+    <p class="help-message" v-show="errorMessage || meta.valid">
+      {{ errorMessage || successMessage }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { useField } from "vee-validate";
+
+export default {
+  props: {
+  },
+  data(){
+      return {
+          
+      }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,7 @@ import router from "./router";
 import store from "./store";
 
 import { createApp } from "vue";
-import {
-  ElButton,
-  ElInput,
-  ElBacktop,
-  ElInfiniteScroll,
-  ElDialog,
-} from "element-plus";
+import { ElBacktop, ElDialog, ElButton } from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -31,13 +25,10 @@ app.config.productionTip = false;
 
 app.use(store);
 app.use(router);
-app.use(ElInfiniteScroll);
 
-app.component(ElButton.name, ElButton);
-app.component(ElInput.name, ElInput);
 app.component(ElBacktop.name, ElBacktop);
-app.component(ElInfiniteScroll.name, ElInfiniteScroll);
 app.component(ElDialog.name, ElDialog);
+app.component(ElButton.name, ElButton);
 app.component("fa-icon", FontAwesomeIcon);
 app.component("base-card", BaseCard);
 
